e2cc04e565c798d187dc92047140a42871c0d345
Aditya Tibrewal <aditya7bgp@gmail.com>
2021-03-19 05:04:43 +0000

    fix

--- reverse-words-in-string-2.js
+++ reverse-words-in-string-2.js
@@ -10,17 +10,17 @@ function reverse(str) {
     if ( str[i] !== ' ') {
       word = word + str[i];
     } else {
-      sentence = word + ' ' + sentence;
+      sentence =  ' ' + word + sentence;
       word = '';
     }
   };
-  sentence = word + ' ' + sentence;
+  sentence =  ' ' + word + sentence;
   return sentence;
 }
 
-let result = reverse('geeks quiz practice code');
+let result = reverse(' geeks quiz practice code');
 console.log(result);
 
 
-let result1 = reverse('getting good at coding needs a lot of practice');
+let result1 = reverse(' getting good at coding needs a lot of practice');
 console.log(result1);
